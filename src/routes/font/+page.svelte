8fe43ccf9740ec849e3c97455713255319f9d4c0
Trulle123 <leonard.trulsson@gmail.com>
2026-06-26 08:27:00 +0200

    center div

--- src/routes/font/+page.svelte
+++ src/routes/font/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Get [Font name]</h1>
 
+<div class="main-container">
+    <div class="font-content">
+        <p class="font-preview">! ? : / " , . 0 1 2 3 4 5 6 7 8 9</p>
+        <p class="font-preview">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Å Ä Ö</p>
+        <p class="font-preview">a b c d e f g h i j k l m n o p q r s t u v w x y z å ä ö</p>
+    </div>
+</div>
